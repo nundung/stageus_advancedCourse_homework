ec3f0f180c2ac605e2c45ca969bd63b83d935250
nundung <jjy809123@naver.com>
2023-12-30 06:18:36 +0000

    db연결 환경변수 설정완료

--- psql.js
+++ psql.js
@@ -1,13 +1,15 @@
 // Import
 // 다른 자바스크립트를 임포트할수 있도록해주는 명령어require
-const { Pool } = require("pg")
+const { Pool } = require("pg") //require()는 CommonJS 모듈 시스템에서 사용되고, import는 ES6 모듈 시스템에서 사용된다
+
+require('dotenv').config()
 
 const pool = new Pool({
-    user: "ubuntu",
-    password: "1234",
-    host: "localhost",
-    database: "week14homework",
-    port: 5432,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    port: process.env.PORT
 })
 
 pool.connect()
