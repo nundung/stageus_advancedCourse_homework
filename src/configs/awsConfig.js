0ba3fc4b0184fd627ba07e43a7f19f5966fb0f58
nundung <jjy809123@naver.com>
2024-01-29 08:33:13 +0000

    미들웨어 함수 합침& 환경변수 추가

--- src/configs/awsConfig.js
+++ src/configs/awsConfig.js
@@ -1,5 +1,10 @@
 //Import
 const AWS = require("aws-sdk")
+const path = require("path")
+const multer = require("multer")
+const multerS3 = require("multer-s3")
+const S3 = new AWS.S3()
+
 
 //aws region 및 자격증명 설정
 AWS.config.update({
@@ -8,6 +13,15 @@ AWS.config.update({
     region: 'ap-northeast-2',
 })
 
-const S3 = new AWS.S3()
+const uploadServer = multer({
+    dest: path.join(__dirname, "../../../uploads/")
+})
 
-module.exports = { S3 }
+const uploadS3 = multer({
+    storage: multerS3({
+        s3: S3,
+        bucket: process.env.S3_BUCKET_NAME
+    })
+})
+
+module.exports = { S3 ,uploadServer, uploadS3  }
---
+++ src/controllers/visitorController.js
@@ -0,0 +1,49 @@
+//Import
+const redis = require("redis").createClient()
+
+const visitorHour = async (req, res, next) => {
+    const result = {
+        "message": "",
+        "data": {
+            "visitor": null
+        }
+    }
+    try {
+        const VISITOR_HOUR_KEY = process.env.VISITOR_HOUR_KEY
+        
+        await redis.connect()
+        const visitor = await redis.sCard(`${VISITOR_HOUR_KEY}`)
+        await redis.disconnect()
+
+        result.data.visitor = visitor
+        res.status(200).send(result)
+    }
+    catch (err) {
+        next (err)
+    }
+}
+
+
+const visitorDay = async (req, res, next) => {
+    const result = {
+        "message": "",
+        "data": {
+            "visitor": null
+        }
+    }
+    try {
+        const VISITOR_DAY_KEY = process.env.VISITOR_DAY_KEY
+
+        await redis.connect()
+        const visitor = await redis.sCard(`${VISITOR_DAY_KEY}`)
+        await redis.disconnect()
+        
+        result.data.visitor = visitor
+        res.status(200).send(result)
+    }
+    catch (err) {
+        next (err)
+    }
+}
+
+module.exports = { visitorHour, visitorDay }
